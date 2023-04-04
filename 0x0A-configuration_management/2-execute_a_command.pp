2239a36e2e98525e15724e1e25ab56c518ab5e09
Mark Kibara <markkibara2014@gmail.com>
2023-04-04 15:36:18 +0300

    Modified 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,8 @@
 # A puppet file that kills a process 'killmenow'
 
 exec { 'killmenow':
-	path => '/usr/bin',
-	command => 'pkill killmenow',
+	path     => '/usr/bin',
+	command  => 'pkill killmenow',
 	provider => 'shell',
-	returns => [0, 1],
+	returns  => [0, 1],
 }
