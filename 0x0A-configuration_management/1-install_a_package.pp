b71261dffdb3dde121e50d6758b89702434c08dc
Mark Kibara <markkibara2014@gmail.com>
2023-04-04 15:35:31 +0300

    Modified 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # A puppet file that installs puppet-lint
 
-package { 'puppet-lint':
-    ensure   => '2.5.0',
-    provider => 'gem',
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
