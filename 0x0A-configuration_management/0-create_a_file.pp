c2c46ce980834f1b3cbb22497f52188cc47a4e9f
Mark Kibara <markkibara2014@gmail.com>
2023-04-04 14:45:46 +0300

    0-create_a_file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -5,5 +5,7 @@ file { '/tmp/school':
 	path => '/tmp/school',
 	mode => '0744',
 	owner => 'www-data',
-	group => 'I love Puppet'
+	group => 'www-data',
+	content => 'I love Puppet\n',
+	ensure_newline => true,
 }
