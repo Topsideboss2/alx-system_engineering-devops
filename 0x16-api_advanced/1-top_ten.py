bb61c679bd38c53b04a831780c736c1f8938ed28
Mark Kibara <markkibara2014@gmail.com>
2024-01-17 18:59:44 +0300

    Modify 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ def top_ten(subreddit):
     """ Function that queries the Reddit API and prints the titles of the
         first 10 hot posts listed for a given subreddit. If not a valid
         subreddit, print None. """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
                AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77\
                Safari/537.36"}
