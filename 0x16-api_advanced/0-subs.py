1e87cef6b358952a4131405e67209c6247f5e07f
Mark Kibara <markkibara2014@gmail.com>
2024-01-17 19:08:25 +0300

    Modify 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,4 +18,5 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         return response.json().get("data").get("subscribers")
-    return 0
+    else:
+        return 0
