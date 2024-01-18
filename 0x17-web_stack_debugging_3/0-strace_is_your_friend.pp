60bef9b992b4a1d84da39a1d333a2328695de829
Mark Kibara <markkibara2014@gmail.com>
2024-01-18 17:13:57 +0300

    Add 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixed wrong extension in wp-settings.php
 
 exec('fix-wp':
-  command => 'sed -i "s/\.php/\.php7/g" /var/www/html/wp-settings.php',
+  command => 'sed -i "s/\.phpp/\.php/g" /var/www/html/wp-settings.php',
   path    => ['/bin', '/usr/bin'],
 )
